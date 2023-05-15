aca640107202ff9440c291a8d8e6e141d79342b7
Sergio Lopez <slp@redhat.com>
2023-05-15 15:44:51 +0200

    create_tap: enable IPv4 forwarding
    Enable IPv4 forwarding in addition to masquerading VMs packets.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- create_tap.sh
+++ create_tap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
 ip tuntap add dev cvd-mtap-01 mode tap group kvm vnet_hdr
 ip link set dev cvd-mtap-01 up
 ip addr add 192.168.97.1/30 broadcast + dev cvd-mtap-01
